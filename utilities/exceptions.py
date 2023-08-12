683f8dd4e4a0649f404ff4e3853f9c5d368ddcc0
siguha <116118668+siguha@users.noreply.github.com>
2023-08-11 22:42:39 -0700

    + LogNotFound exception for missing logs in sheet.

--- utilities/exceptions.py
+++ utilities/exceptions.py
@@ -2,3 +2,7 @@ class Exceptions:
     class UserNotFound(Exception):
         """Raised when a given DiscordID is not found in the database."""
         pass
+
+    class LogNotFound(Exception):
+        """Raised when an AAR log is not found in the AAR sheet."""
+        pass

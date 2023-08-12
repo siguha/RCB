83b10b0801f58e32f4e9ea4310ae2f3c74cfdf35
siguha <116118668+siguha@users.noreply.github.com>
2023-08-12 13:49:17 -0700

    + LOAInvalid exception.

--- utilities/exceptions.py
+++ utilities/exceptions.py
@@ -6,3 +6,8 @@ class Exceptions:
     class LogNotFound(Exception):
         """Raised when an AAR log is not found in the AAR sheet."""
         pass
+    
+    class LOAInvalid(Exception):
+        """RAised when an LOA is invalid."""
+        pass
+    

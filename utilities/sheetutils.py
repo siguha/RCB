761d65231ab9a1748963a577c095027280edc93b
siguha <116118668+siguha@users.noreply.github.com>
2023-08-11 22:43:23 -0700

    + AAR utils class, expanded last_aar() for days.

--- utilities/sheetutils.py
+++ utilities/sheetutils.py
@@ -1,5 +1,4 @@
 import gspread
-import discord
 import asyncio
 import aiosqlite
 from datetime import datetime
@@ -10,21 +9,17 @@ OFFICER = SA.open_by_url('https://docs.google.com/spreadsheets/d/1k4p4lUivqTXYhW
 PUBLIC = SA.open_by_url('https://docs.google.com/spreadsheets/d/1PBPZOHxNSw9hzSErpFxQ6E_OtyNKTptTK7M6BmG3-J8/')
 AAR = SA.open_by_url('https://docs.google.com/spreadsheets/d/1dQso9v7GN4yXE5MsiTeS4tJdHQZKbR77ZXymM-wJUPg/')
 AARs = AAR.worksheet('AAR Logs')
+TRYOUTS = PUBLIC.worksheet('Tryouts')
 ROSTER = PUBLIC.worksheet('Roster')
 DATABASE = OFFICER.worksheet('Officer View')
 IDS = OFFICER.worksheet('IDs')
 e = Exceptions()
 
-# def run_and_get(coro):
-#     task = asyncio.create_task(coro)
-#     asyncio.get_running_loop().run_until_complete(task)
-#     return task.result()
-
 class SheetUtilities:
     """Sheet utilities for the RCBot."""
 
     async def id_fetch(self, did: int) -> str:
-        db = await aiosqlite.connect('ids.db')
+        db = await aiosqlite.connect('rcb.db')
 
         async with db.cursor() as cursor:
             await cursor.execute('''SELECT steamid FROM ids WHERE did = ?''', (did,))
@@ -38,11 +33,7 @@ class SheetUtilities:
         except:
             raise e.UserNotFound('SteamID not found in database. Profile not synced.')
 
-    # async def bulk_id_fetch(self, dids: list[int]) -> list[str]:
-    #     db = await aiosqlite.connect('ids.db')
-    #     ids 
-
-    async def last_row(self, sheet: gspread.Worksheet, col: int = 2) -> None:
+    async def last_row(self, sheet: gspread.Worksheet, col: int = 2) -> str:
         """Fetches the last row of a column.
         
         Parameters
@@ -64,11 +55,14 @@ class SheetUtilities:
 
         return size
     
-    async def last_arr(self, did: int) -> str:
+    async def last_arr(self, did: int) -> tuple[datetime, int]:
         steamid = await self.id_fetch(did)
         row = AARs.find(steamid)
-        date = AARs.acell(f'A{row.row}').value
-        return date
+        date = datetime.strptime(AARs.acell(f'A{row.row}').value, '%m/%d/%Y %H:%M:%S')
+        today = datetime.now()
+        days = (today - date).days
+
+        return date, days
 
     class ProfileUtils:
         """Profile Utilities class of SheetUtilities."""
@@ -81,13 +75,14 @@ class SheetUtilities:
             self.utils = SheetUtilities()
 
         async def db_connect(self) -> None:
-            db = await aiosqlite.connect('ids.db')
+            db = await aiosqlite.connect('rcb.db')
 
             async with db.cursor() as cursor:
                 await cursor.execute('''CREATE TABLE IF NOT EXISTS ids (did INTEGER PRIMARY KEY, 
                                     steamid TEXT, 
                                     name TEXT)''')
             await db.commit()
+            await db.close()
     
         async def profile_sync(self, steamid: str, discordid: str, name: str) -> None:
             """Syncs a user's Discord ID to their SteamID, storing it locally as well as on a sheet.
@@ -101,14 +96,14 @@ class SheetUtilities:
             name: :class:`str`
                 The user's Discord display name at the time of call.
             """
-            db = await aiosqlite.connect('ids.db')
+            db = await aiosqlite.connect('rcb.db')
             last_row = await self.utils.last_row(IDS, 1)
 
             async with db.cursor() as cursor:
                 await cursor.execute('''INSERT INTO ids (did, steamid, name) VALUES (?, ?, ?)
                                         ON CONFLICT(did) DO
                                         UPDATE SET steamid = excluded.steamid, name = excluded.name''', (discordid, steamid, name))
-                await db.commit()
+            await db.commit()
             await db.close()
 
             existing = IDS.find(steamid)
@@ -116,15 +111,13 @@ class SheetUtilities:
                 IDS.batch_update([{
                     'range' : f'A{last_row}:C{last_row}',
                     'values' : [[name, steamid, str(discordid)]],
-
                 }], value_input_option = 'RAW')
 
             else:
                 IDS.batch_update([{
                     'range' : f'A{existing.row}:C{existing.row}',
                     'values' : [[name, steamid, str(discordid)]],
-
-                }], value_input_option = 'USER_ENTERED')
+                }], value_input_option = 'RAW')
 
         async def stats_fetch(self, did: int) -> dict[str]:
             row = DATABASE.find(str(did))
@@ -133,7 +126,7 @@ class SheetUtilities:
                 raise e.UserNotFound('User not found in Officer Database.')
             
             else:
-                last_aar = await self.utils.last_arr(did)
+                last_aar, days = await self.utils.last_arr(did)
                 vals = DATABASE.row_values(row.row)
                 data = {
                     'PROMO_DATE' : vals[8],
@@ -145,7 +138,11 @@ class SheetUtilities:
                     'LEADS' : vals[17],
                     'SGT_TRIALS' : vals[18],
                     'BT_TRIALS' : vals[19],
-                    'LAST_AAR' : last_aar
+                    'LAST_AAR' : last_aar.strftime("%m/%d/%Y %H:%M:%S"),
+                    'LAST_AAR_DAYS' : days,
+                    'LAST_SEEN' : vals[6],
+                    'LAST_SEEN_DAYS' : vals[7],
+                    'LOA' : vals[22]
                 }
 
                 return data
@@ -176,6 +173,118 @@ class SheetUtilities:
                 return data
 
     class AarUtils:
-        ...
+        """AAR Utilities class of SheetUtilities."""
+
+        def __init__(self):
+            loop = asyncio.get_running_loop()
+            loop.create_task(self.db_connect())
+            self.utils = SheetUtilities()
+
+        async def db_connect(self) -> None:
+            db = await aiosqlite.connect('rcb.db')
+
+            async with db.cursor() as cursor:
+                await cursor.execute('''CREATE TABLE IF NOT EXISTS aars (log_id INTEGER PRIMARY KEY, 
+                                    msg_id INTEGER)''')
+            await db.commit()
+            await db.close()
+
+        async def aar_cache(self, log_id: int, msg_id: int):
+            db = await aiosqlite.connect('rcb.db')
+
+            async with db.cursor() as cursor:
+                await cursor.execute('''INSERT INTO aars (log_id, msg_id) VALUES (?, ?)''', (log_id, msg_id))
+
+            await db.commit()
+            await db.close()
+
+        async def aar_remove(self, log_id: int):
+            db = await aiosqlite.connect('rcb.db')
+
+            async with db.cursor() as cursor:
+                await cursor.execute('''DELETE FROM aars WHERE log_id = ?''', (log_id,))
+            
+            await db.commit()
+            await db.close()
+
+            log_loc = AARs.find(str(log_id))
+            try:
+                AARs.delete_row(log_loc.row)
+
+            except AttributeError:
+                raise e.LogNotFound(f'LogID `{log_id}` not found in AAR sheet.')
+
+        async def aar_fetch(self, log_id: int):
+            db = await aiosqlite.connect('rcb.db')
+
+            async with db.cursor() as cursor:
+                await cursor.execute('''SELECT msg_id FROM aars WHERE log_id = ?''', (log_id,))
+                msg_id = await cursor.fetchone()
+
+            await db.close()
+            return msg_id[0]
+
+        async def aar_create(self, *args, msg_id: int, log_id: int, log: str, logger_id: int, users: list[int] = None):
+            await self.aar_cache(log_id, msg_id)
+            date = datetime.now().strftime("%m/%d/%Y %H:%M:%S")
+            
+            if log == 'Lead':
+                AARs.insert_row(values = [date, str(log_id), log, str(logger_id), args[0], args[1], args[2], args[3]], value_input_option = 'USER_ENTERED', index = 3)
+
+            elif log == 'Lead Spectate':
+                AARs.insert_row(values = [date, str(log_id), log, str(logger_id), args[0], args[1], args[2], args[3]], value_input_option = 'USER_ENTERED', index = 3)   
+
+            elif log == 'Training':
+                AARs.insert_row(values = [date, str(log_id), log, str(logger_id)], value_input_option = 'USER_ENTERED', index = 3)   
+                AARs.append_row(values = [args[0], args[1], args[2], args[3], args[4]], value_input_option = 'USER_ENTERED', insert_data_option = 'OVERWRITE', table_range = 'I3:M3')
+            
+            elif log == 'Lore Activity':
+                AARs.insert_row(values = [date, str(log_id), log, str(logger_id)], value_input_option = 'USER_ENTERED', index = 3)   
+                AARs.append_row(values = [args[0], args[1], args[2]], value_input_option = 'USER_ENTERED', insert_data_option = 'OVERWRITE', table_range = 'N3:P3')
+
+            elif log == 'BT':
+                AARs.insert_row(values = [date, str(log_id), log, str(logger_id)], value_input_option = 'USER_ENTERED', index = 3)   
+                AARs.append_row(values = [args[0], args[1], args[2]], value_input_option = 'USER_ENTERED', insert_data_option = 'OVERWRITE', table_range = 'Q3:S3')               
+
+            elif log == 'SGT Trials':
+                AARs.insert_row(values = [date, str(log_id), log, str(logger_id)], value_input_option = 'USER_ENTERED', index = 3)   
+                AARs.append_row(values = [args[0], args[1], args[2]], value_input_option = 'USER_ENTERED', insert_data_option = 'OVERWRITE', table_range = 'T3:V3')
+
+            elif log == 'Spec Cert':
+                AARs.insert_row(values = [date, str(log_id), log, str(logger_id)], value_input_option = 'USER_ENTERED', index = 3)   
+                AARs.append_row(values = [args[0], args[1], args[2]], value_input_option = 'USER_ENTERED', insert_data_option = 'OVERWRITE', table_range = 'W3:Y3')
+
+            elif log == 'Tryout':
+                last_row = await self.utils.last_row(TRYOUTS)
+                date = datetime.now().strftime("%m/%d/%Y")
+                AARs.insert_row(values = [date, str(log_id), log, str(logger_id)], value_input_option = 'USER_ENTERED', index = 3)   
+                AARs.append_row(values = [args[1], args[2], args[3]], value_input_option = 'USER_ENTERED', insert_data_option = 'OVERWRITE', table_range = 'Z3:AB3')
+                TRYOUTS.append_row(values = [date, args[0], args[3], args[1], args[2]], value_input_option = 'USER_ENTERED', insert_data_option = 'OVERWRITE', table_range = f'B{int(last_row)}:F{int(last_row)}')
+
+            if users is not None:
+                col = 29
+                for user in users:
+                    AARs.update_cell(3, col, str(user))
+                    col += 1
+
+        async def aar_append(self, log_id: int, members: list[str]):
+            loc = AARs.find(str(log_id))
+
+            if loc is None:
+                raise e.LogNotFound(f'LogID `{log_id}` not found in AAR sheet.')
+            else:
+                row = loc.row
+                length = len(AARs.row_values(row))
+                col = (length + 1) if length >= 29 else 29
+
+                for member in members:
+                    AARs.update_cell(row, col, member)
+                    col += 1
         
+        async def aar_pop(self, log_id: int, user: int):
+            log_loc = AARs.find(str(log_id))
+            row_vals = AARs.row_values(log_loc.row)
+            col = row_vals.index(str(user)) + 1
+            AARs.update_cell(log_loc.row, col, '')
+
 # https://discord.com/api/oauth2/authorize?client_id=1109688912026288168&permissions=8&scope=bot%20applications.commands

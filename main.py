6a7b0a07284cd902550595418d2f8a79bc9dd360
siguha <116118668+siguha@users.noreply.github.com>
2023-08-18 17:37:22 -0700

    Legacy prefix to !, cog loading, button views

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from discord.ext import commands
 import os
 from dotenv import load_dotenv
 
+from cogs.forms import loa
+from cogs.forms import tickets
+
 """
 Written by @sigRao for Superior Severs CWRP
 
@@ -17,20 +20,27 @@ class RCBot(commands.Bot):
     def __init__(self) -> None:
         super().__init__(
             intents = discord.Intents.all(),
-            command_prefix = "c. ",
+            command_prefix = "!",
             owner_id = 484932458538860544
         )
 
         self.init_extensions = [
             'cogs.admin',
+            'cogs.misc',
             'cogs.forms.aar',
             'cogs.forms.loa',
+            'cogs.forms.tickets',
+            'cogs.users.officer',
             'cogs.users.profile',
         ]
 
     async def setup_hook(self) -> None:
         for ext in self.init_extensions:
             await self.load_extension(ext)
+
+        self.add_view(loa.Views.ReturnButtons())
+        self.add_view(tickets.support_button())
+
 
     async def on_ready(self) -> None:
         print(f"Connected as {self.user}")

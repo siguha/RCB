2d4dab2dddc2985e784090b20d224ff95bf9b104
siguha <116118668+siguha@users.noreply.github.com>
2023-12-05 20:06:59 -0800

    !chaind for chained msg deletes, role checks

--- cogs/admin.py
+++ cogs/admin.py
@@ -15,6 +15,7 @@ class Admin(commands.Cog):
         self.miscUtils = SheetUtilities.MiscUtils()
     
     @commands.hybrid_command(name = "load", with_app_command = False)
+    @app_commands.checks.has_any_role(333432981605580800, 452534405874057217)
     async def load(self, ctx: commands.Context, *, module: str):
         """Loads a module."""
         try:
@@ -28,6 +29,7 @@ class Admin(commands.Cog):
             await ctx.send('Extension successfully loaded.', delete_after = 5)
 
     @commands.hybrid_command(name = "unload", with_app_command = False)
+    @app_commands.checks.has_any_role(333432981605580800, 452534405874057217)
     async def unload(self, ctx: commands.Context, *, module: str):
         """Unloads a module."""
         try:
@@ -41,6 +43,7 @@ class Admin(commands.Cog):
             await ctx.send('Extension successfully unloaded.', delete_after = 5)
 
     @commands.hybrid_command(name = "reload", with_app_command = False)
+    @app_commands.checks.has_any_role(333432981605580800, 452534405874057217)
     async def _reload(self, ctx: commands.Context, *, module: str):
         """Reloads a module."""
         try:
@@ -54,6 +57,7 @@ class Admin(commands.Cog):
             await ctx.send('Extension successfully reloaded.', delete_after = 5)
 
     @commands.hybrid_command(name = "sync", with_app_command = False)
+    @app_commands.checks.has_any_role(333432981605580800, 452534405874057217)
     async def reload(self, ctx: commands.Context):
         await self.client.tree.sync()
         await ctx.message.delete()
@@ -64,11 +68,11 @@ class Admin(commands.Cog):
         await ctx.message.delete()
         await ctx.channel.send("https://tenor.com/view/star-wars-yeet-darth-vader-fallen-order-jedi-gif-24487821")
 
-    @commands.hybrid_command(name="spec aar", with_app_command=False)
+    @commands.hybrid_command(name="spec_aar", with_app_command=False)
     async def spec(self, ctx: commands.Context):
         await ctx.channel.send("## Logging a Spec AAR?\nGo into <#343795194333757441> and type `/aar`.\n\nSelect **Spec Certs** and **testee** as the optional parameter. Select whoever the testee was in the member select menu that pops up above.\n\nFill out the logging form as directed, and done.\n\n*If you make a mistake, you'll need to contact an NCO+ to get it fixed, alongside the necessary corrections.")
 
-    @commands.hybrid_command(name="lore aar", with_app_command=False)
+    @commands.hybrid_command(name="jedi_aar", with_app_command=False)
     async def lore(self, ctx: commands.Context):
         await ctx.channel.send("## Logging a Lore AAR?\nGo into <#343795194333757441> and type `/aar`.\n\nSelect **Lore Activities**.\n\nFill out the logging form as directed.\n\nSelect the participants, being the RC present. If it was just yourself, you can select only yourself as the participant.\n\n*If you make a mistake, you'll need to contact an NCO+ to get it fixed, alongside the necessary corrections.")       
 
@@ -77,6 +81,25 @@ class Admin(commands.Cog):
         data = await self.miscUtils.retention_fetch()
 
         await ctx.channel.send(f"# Retention Stats\n**Analysis Period**: __{data['period']}__\n- **Headcount**: `{data['headcount']}`\n- **Prev. Headcount**: `{data['headcount_old']}`\n- **Intake**: `{data['intake']}`\n\n**Retention Rate**: `{data['retention']}`")
+
+    @commands.hybrid_command(name="chaind", with_app_command=False)
+    @app_commands.checks.has_any_role(333432981605580800, 452534405874057217)
+    async def chain_delete(self, ctx: commands.Context, *args: str):
+        not_found = []
+        not_found_count = 0
+
+        for arg in args:
+            try:
+                message = await ctx.fetch_message(arg)
+                await message.delete()
+
+            except Exception as e:
+                not_found_count += 1
+                not_found.append(f'\n- `{arg}: {e.__class__.__name__}`')
+        
+        deleted_count = len(args) - not_found_count
+        
+        await ctx.send(f"**{len(args)}** Message IDs processed, **{deleted_count if deleted_count > 0 else 0}** Messages deleted, **{not_found_count}** Message IDs not found.\n\nMissing IDs: {''.join(not_found)}")
 
 async def setup(client: commands.Bot) -> None:
     await client.add_cog(Admin(client))

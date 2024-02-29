e7fffd89926f1e57f424e8d7804c71aabcf4b953
siguha <116118668+siguha@users.noreply.github.com>
2024-02-29 10:53:36 -0800

    expanded upon squads base, added promotion support
    includes:
    - color
    - sticking squads so they aren't deleted
    - better rename

--- cogs/squads.py
+++ cogs/squads.py
@@ -21,10 +21,21 @@ class Squads(commands.GroupCog, name = "squad", description = "Squad Commandset.
 
     @app_commands.command(name='rename', description='Forcefully rename a squad.')
     @app_commands.checks.has_any_role(842312625735860255, 333432981605580800, 452534405874057217)
-    async def rename(self, interaction: discord.Interaction, squad: str, name: str):
+    async def cmd_rename(self, interaction: discord.Interaction, squad: str, name: str, color: str = None):
         try:
-            await self.utils.squad_rename(interaction.guild, squad, name)
+            await self.utils.squad_rename(interaction.guild, squad, name, color)
+            
             await interaction.response.send_message(content=f"**{squad}** has been renamed to **{name}**.")
+
+        except Exceptions.SquadNotFound as e: 
+            await interaction.response.send_message(content=f'{e.__class__.__name__}: {e}', delete_after=10, ephemeral=True)
+
+    @app_commands.command(name="stick", description="Stick a squad for legacy purposes.")
+    @app_commands.checks.has_any_role(842312625735860255, 333432981605580800, 452534405874057217)
+    async def cmd_stick(self, interaction: discord.Interaction, squad: str):
+        try:
+            await self.utils.squad_stick(squad=squad)
+            await interaction.response.send_message(content=f"**{squad}** has been stuck.")
 
         except Exceptions.SquadNotFound as e: 
             await interaction.response.send_message(content=f'{e.__class__.__name__}: {e}', delete_after=10, ephemeral=True)
@@ -38,6 +49,20 @@ class Squads(commands.GroupCog, name = "squad", description = "Squad Commandset.
     async def squad_check(self) -> None:
         guild = self.client.get_guild(333429464752979978)
         await self.utils.squad_builder(guild)
+
+class EditModal(ui.Modal, title = "Squad Promotion"):
+    def __init__(self):
+        super().__init__(timeout = None)
+        self.vals = {}
+
+    name = ui.TextInput(label = "New Squad Name", style = discord.TextStyle.short, placeholder="Alpha",required = True)     
+    role_color = ui.TextInput(label = "Role HEX Color", style = discord.TextStyle.short, placeholder="#FFFFFF", required = True)
+
+    async def on_submit(self, interaction: discord.Interaction):
+        await interaction.response.defer()
+        self.vals['NAME'] = self.name.value
+        self.vals['COLOR'] = self.role_color.value
+        self.stop()
 
 async def setup(client: commands.Bot) -> None:
     await client.add_cog(Squads(client))

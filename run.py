df42811834e6364756ae817109d4904682d4298f
siguha <116118668+siguha@users.noreply.github.com>
2024-04-17 13:32:58 -0700

    establishing db connection in setup

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ import discord
 # import os
 
 from discord.ext import commands
+from GSClient import GSClient
 # from cogs.form
 
 class AZI(commands.Bot):
@@ -12,9 +13,16 @@ class AZI(commands.Bot):
             owner_id=484932458538860544
         )
 
+        # Intilaizing the GSClient
+        self.gs_client = GSClient()
+
         self.__initial_extensions = []
 
     async def setup_hook(self):
+        # Setting up our database connection
+        await self.gs_client.setup()
+
+        # Loading cog extensions
         for extension in self.__initial_extensions:
             self.load_extension(extension)
 
@@ -22,6 +30,10 @@ class AZI(commands.Bot):
         print(f'Logged in as {self.user} ({self.user.id})')
 
         await client.change_presence(activity=discord.Game(name='v3 - BB ON TOP 💯'))
+
+    async def close(self):
+        await self.gs_client.teardown()
+        await super().close()
     
 client = AZI()
 client.run()

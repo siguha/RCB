aef302473099e0f4b5a618854e9489c61490f22c
siguha <116118668+siguha@users.noreply.github.com>
2024-04-19 21:10:02 -0700

    table creation on startup, create classmethod

--- GSClient/database_manager.py
+++ GSClient/database_manager.py
@@ -5,9 +5,17 @@ class DatabaseManager:
         self.database_path = database_path
         self.connection = None
 
+    @classmethod
+    async def create(cls, database_path):
+        self = cls(database_path)
+        await self.connect()
+        await self.initialize_db()
+        return self
+
     async def connect(self):
         if not self.connection:
-            self.connection = await aiosqlite.connect(self.database_path)\
+            self.connection = await aiosqlite.connect(self.database_path)
+            print("Database Connection Established.")
     
     async def get_cursor(self):
         return await self.connection.cursor()
@@ -16,3 +24,47 @@ class DatabaseManager:
         if self.connection:
             await self.connection.close()
             self.connection = None
+            print("Database Connection Terminated.")
+
+    async def test_cursor(self):
+        async with self.connection.cursor() as cursor:
+            await cursor.execute('SELECT 1;')
+            print(await cursor.fetchone())    
+
+    async def initialize_db(self):
+        table_creation_commands = [
+            """
+            CREATE TABLE IF NOT EXISTS ids (
+                did INTEGER PRIMARY KEY,
+                steamid TEXT,
+                batch TEXT,
+                designation TEXT,
+                name TEXT
+            );
+            """,
+            """
+            CREATE TABLE IF NOT EXISTS aars (
+                log_id INTEGER PRIMARY KEY, 
+                msg_id INTEGER
+            );
+            """,
+            """
+            CREATE TABLE IF NOT EXISTS squads (
+                squad_name TEXT PRIMARY KEY,
+                role_id INTEGER,
+                channel_id INTEGER,
+                mem1 INTEGER,
+                mem2 INTEGER,
+                mem3 INTEGER,
+                mem4 INTEGER,
+                mem5 INTEGER,
+                persistent TEXT
+            );
+            """
+        ]
+
+        async with self.connection.cursor() as cursor:
+            for command in table_creation_commands:
+                await cursor.execute(command)
+            
+            await self.connection.commit()
